12ac392f4e2b3afa6cf1378103641833325ec592
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2022-11-21 13:43:10 +0000

    video ads added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import MovieCard from "../MovieCard/MovieCard";
 import "./Home.css";
 import Sidebar from "../Sidebar/Sidebar";
+import Movies from "../Movies/Movies";
 
 function Home() {
   return (
     <div className="home__container">
       <Sidebar/>
-
+<Movies/>
       <MovieCard />
     </div>
   );
