3144140032061d9aa964547d4e0dd0924a5afe63
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2022-11-30 16:26:42 +0000

    search filter added

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -8,76 +8,95 @@ import CircularProgress from "@mui/material/CircularProgress";
 import Movies from "../Movies/Movies";
 import ReactPaginate from "react-paginate";
 import "./MovieCard.css";
-import { TXXX_DATA } from "../../TXXX_DATA";
+import axios from "axios";
 
 function MovieCard() {
-    // const vidoeData = async () => await fetch(  "TXXX_DATA.json");
-  const [movieItem, setMovieItem] = useState(TXXX_DATA);
+  // const vidoeData = async () => await fetch(  "TXXX_DATA.json");
+  const [movieItem, setMovieItem] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { selectMovies, moviesOrder } = useContext(MovieContext);
   const [page, setPage] = useState(1);
   const [searchMovies, setSearchMovies] = useState();
-  const moviesPerPage = 10;
+  const [sortValue, setSortValue] = useState(" ");
+  const moviesPerPage = 30;
   const pagesVisited = page * moviesPerPage;
 
-  // const movieFetchBox = async () => {
-  //   setIsLoading(true);
-  //   await fetch(
-  //     `https://www.eporner.com/api/v2/video/search/?query=${selectMovies}&per_page=10000&page=80&thumbsize=big&order=${moviesOrder}&gay=0&lq=1&format=json`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setMovieItem(data));
-  //   setIsLoading(false);
-  // };
-  // const vidoeData = async () => TXXX_DATA;
-  // const movieFetchBox = async () => {
-  //   setIsLoading(true);
-  //   await fetch( TXXX_DATA)
-  //     .then((res) => res.json())
-  //     .then((data) => setMovieItem(data));
-  //   setIsLoading(false);
-  // };
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   movieFetchBox()
-  //   setIsLoading(false);
-  // }, []);
-  console.log(movieItem)
-  
+  useEffect(() => {
+    loadMovieData();
+  }, []);
 
-  
-  // const [pages, setPages] = useState(moviesOrder)
-  const pageCount = Math.ceil(movieItem.length/10);
+  const sortMovies = [
+    "categories",
+    "description",
+    "title",
+    "duration",
+    "lesbian",
+  ];
 
-  const searchHandler = () => {
-    return (
-      movieItem.videos &&
-      movieItem.videos.filter(
-        (movies) =>
-          movies.title.toLowerCase().includes(searchMovies) ||
-          movies.keywords.toLowerCase().includes(searchMovies)
-      )
-    );
+  const loadMovieData = async () => {
+    return await axios
+      .get("http://localhost:5000/videos")
+      .then((response) => setMovieItem(response.data))
+      .catch((err) => console.log(err));
+  };
+  console.log(movieItem);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    return await axios
+      .get(`http://localhost:5000/videos?q=${searchMovies}`)
+      .then((response) => {
+        setMovieItem(response.data);
+        setSearchMovies("");
+      })
+      .catch((err) => console.log(err));
   };
 
-  const displayMovies =
-    movieItem.length > 0 && movieItem.slice(pagesVisited, pagesVisited + moviesPerPage) 
-    .map((movies) => <Movies {...movies} key={movies.id} />);
+  const handleSort = async (e) => {
+    let value = e.target.value;
+    setSortValue(value);
+    return await axios
+      .get(`http://localhost:5000/videos?_sort=${value}&_order=dsc`)
+      .then((response) => {
+        setMovieItem(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const pageCount = Math.ceil(movieItem.length / 30);
 
+  const displayMovies =
+    movieItem.length > 0 &&
+    movieItem
+      .slice(pagesVisited, pagesVisited + moviesPerPage)
+      .map((movies) => <Movies {...movies} key={movies.id} />);
 
   const pagesHandler = (e, k) => {
     setPage(k);
   };
- 
+
   const changePage = ({ selected }) => {
     setPage(selected);
   };
   return (
     <>
       <div className="movie__container">
-      <input type="search" onChange={searchHandler()} />
+        <form action="" onSubmit={handleSearch}>
+          <select name="" id="" value={sortValue} onChange={handleSort}>
+            <option value="">select value</option>
+            {sortMovies.map((item, index) => (
+              <option value={item} key={index}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <input
+            type="search"
+            value={searchMovies}
+            onChange={(e) => setSearchMovies(e.target.value)}
+          />
+        </form>
         <div className="movie__display">
-          {isLoading ? <CircularProgress color="secondary" /> :(displayMovies)}
+          {isLoading ? <CircularProgress color="secondary" /> : displayMovies}
 
           {/* <Movies {...movieItem} key={ movieItem}  /> */}
         </div>
--- src/components/Searchbox/Searchbox.jsx
+++ src/components/Searchbox/Searchbox.jsx
@@ -1,9 +1,24 @@
 import { Divider, IconButton, InputBase, Paper } from '@mui/material';
 import SearchIcon from "@mui/icons-material/Search";
+import axios from 'axios';
+import { useState } from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({setMovieItem}) => {
+  const [searchMovies, setSearchMovies] = useState();
+
+    const handleSearch = async (e) => {
+      e.preventDefault();
+      return await axios
+        .get(`http://localhost:5000/videos?q=${searchMovies}`)
+        .then((response) => {
+          setMovieItem(response.data);
+          setSearchMovies("");
+        })
+        .catch((err) => console.log(err));
+    };
   return (
     <Paper
+      onSubmit={handleSearch}
       component="form"
       sx={{ p: "2px 4px", display: "flex", alignItems: "center", minWidth: 80 }}
       size="small"
@@ -12,6 +27,8 @@ const Searchbox = () => {
         sx={{ ml: 0.5, flex: 1 }}
         placeholder="Search Porn Here"
         inputProps={{ "aria-label": "search google maps" }}
+        value={searchMovies}
+        onChange={(e)=>setSearchMovies(e.target.value)}
       />
       <IconButton type="button" sx={{ p: "10px" }}></IconButton>
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
